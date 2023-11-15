0ac1b757b6282de274aecfa63a8197b9ca62c67d
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-11-15 13:27:25 +0200

    add header (not responsible)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import css from "./Header.module.scss";
 import icons from "../../assets/images/icons/icons.svg";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className={css.topper}>
-        <div>
+        <div className={css.topper_container}>
           <ul className={css.list}>
             <li className={css.item}>
               <p>(+216) 70 253 734</p>
@@ -20,46 +21,78 @@ const Header = () => {
               </svg>
             </li>
           </ul>
-        </div>
-        <div className={css.wrapper}>
-          <ul className={css.socials}>
-            <li>
-              <svg className={css.icon}>
-                <use href={icons + "#facebook"}></use>
-              </svg>
-            </li>
-            <li>
-              <svg className={css.icon}>
-                <use href={icons + "#google-plus"}></use>
-              </svg>
-            </li>
-            <li>
-              <svg className={css.icon}>
-                <use href={icons + "#linkedin"}></use>
-              </svg>
-            </li>
-            <li>
-              <svg className={css.icon}>
-                <use href={icons + "#twitter"}></use>
-              </svg>
-            </li>
-          </ul>
-          <ul className={css.list}>
-            <li className={css.item}>
-              <p>تواصل معنا :</p>
-            </li>
-            <li className={css.item}>
-              <button className={css.button_icon}>
+          <div className={css.wrapper}>
+            <ul className={css.socials}>
+              <li>
                 <svg className={css.icon}>
-                  <use href={icons + "#chevron-down"}></use>
+                  <use href={icons + "#facebook"}></use>
                 </svg>
-              </button>
-              <p> العربي</p>
-            </li>
-          </ul>
+              </li>
+              <li>
+                <svg className={css.icon}>
+                  <use href={icons + "#google-plus"}></use>
+                </svg>
+              </li>
+              <li>
+                <svg className={css.icon}>
+                  <use href={icons + "#linkedin"}></use>
+                </svg>
+              </li>
+              <li>
+                <svg className={css.icon}>
+                  <use href={icons + "#twitter"}></use>
+                </svg>
+              </li>
+            </ul>
+            <ul className={css.list}>
+              <li className={css.item}>
+                <p>تواصل معنا :</p>
+              </li>
+              <li className={css.item}>
+                <button className={css.button_icon}>
+                  <svg className={css.icon}>
+                    <use href={icons + "#chevron-down"}></use>
+                  </svg>
+                </button>
+                <p> العربي</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className={css.content}></div>
+
+      <div className={css.content}>
+        <div className={css.content_container}>
+          {/* Search button */}
+          <button className={css.search_button}>
+            <div className={css.search_box}>
+              <i class="kipso-icon-magnifying-glass"></i>
+            </div>
+          </button>
+
+          <ul className={css.sign_list}>
+            <li>
+              {/* register */}
+              <button className={css.register_button}>سجل الآن</button>
+            </li>
+            <li>
+              {/* login  */}
+              <button className={css.login_button}>تسجيل الدخول</button>
+            </li>
+          </ul>
+          <nav>
+            <ul className={css.nav_list}>
+              <li>
+                <NavLink>المحتوى</NavLink>
+              </li>
+              <li>
+                <NavLink>من نحن؟</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div>LOGO</div>
+        </div>
+      </div>
     </header>
   );
 };
