81f2b84b67fcbfdcb52bc7b16e2a4035b46b7118
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-11-17 12:35:08 +0200

    add hover effects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,39 +9,75 @@ const Header = () => {
         <div className={css.topper_container}>
           <ul className={css.list}>
             <li className={css.item}>
-              <p>(+216) 70 253 734</p>
-              <svg className={css.icon}>
-                <use href={icons + "#mobile"}></use>
-              </svg>
+              <a href="tel:+21670253734" className={css.contact_link}>
+                <p className={css.link_content}>(+216) 70 253 734</p>
+                <svg className={css.icon}>
+                  <use href={icons + "#mobile"}></use>
+                </svg>
+              </a>
             </li>
+            <li className={css.item_devider}></li>
             <li className={css.item}>
-              <p>contact@birsaplatform.com</p>
-              <svg className={css.icon}>
-                <use href={icons + "#envelope"}></use>
-              </svg>
+              <a
+                href="mailto:contact@birsaplatform.com"
+                className={css.contact_link}
+              >
+                <p className={css.link_content}>contact@birsaplatform.com</p>
+                <svg className={css.icon}>
+                  <use href={icons + "#envelope"}></use>
+                </svg>
+              </a>
             </li>
           </ul>
           <div className={css.wrapper}>
             <ul className={css.socials}>
               <li>
-                <svg className={css.icon}>
-                  <use href={icons + "#facebook"}></use>
-                </svg>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  className={css.social_link}
+                >
+                  <svg className={css.icon}>
+                    <use href={icons + "#facebook"}></use>
+                  </svg>
+                </a>
               </li>
               <li>
-                <svg className={css.icon}>
-                  <use href={icons + "#google-plus"}></use>
-                </svg>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  className={css.social_link}
+                >
+                  <svg className={css.icon}>
+                    <use href={icons + "#google-plus"}></use>
+                  </svg>
+                </a>
               </li>
               <li>
-                <svg className={css.icon}>
-                  <use href={icons + "#linkedin"}></use>
-                </svg>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  className={css.social_link}
+                >
+                  <svg className={css.icon}>
+                    <use href={icons + "#linkedin"}></use>
+                  </svg>
+                </a>
               </li>
               <li>
-                <svg className={css.icon}>
-                  <use href={icons + "#twitter"}></use>
-                </svg>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  className={css.social_link}
+                >
+                  <svg className={css.icon}>
+                    <use href={icons + "#twitter"}></use>
+                  </svg>
+                </a>
               </li>
             </ul>
             <ul className={css.list}>
@@ -53,8 +89,8 @@ const Header = () => {
                   <svg className={css.icon}>
                     <use href={icons + "#chevron-down"}></use>
                   </svg>
+                  <p> العربي</p>
                 </button>
-                <p> العربي</p>
               </li>
             </ul>
           </div>
@@ -80,17 +116,23 @@ const Header = () => {
               <button className={css.login_button}>تسجيل الدخول</button>
             </li>
           </ul>
-          <nav>
+          <nav className={css.nav}>
             <ul className={css.nav_list}>
               <li>
-                <NavLink>المحتوى</NavLink>
+                <NavLink to="/categories" className={css.nav_link}>
+                  المحتوى
+                </NavLink>
               </li>
               <li>
-                <NavLink>من نحن؟</NavLink>
+                <NavLink to="/about" className={css.nav_link}>
+                  من نحن؟
+                </NavLink>
               </li>
             </ul>
+            <NavLink to="/" className={css.nav_link}>
+              LOGO
+            </NavLink>
           </nav>
-          <div>LOGO</div>
         </div>
       </div>
     </header>
--- src/components/RequestedCourses/RequestedCourses.jsx
+++ src/components/RequestedCourses/RequestedCourses.jsx
@@ -1,80 +1,91 @@
 import { Link } from "react-router-dom";
 import css from "./RequestedCourses.module.scss";
+import hero_image from "../../assets/images/hero/image.png";
 
 const RequestedCourses = () => {
   return (
     <>
       <section className={css.heading_section}>
-        <h2>
-          المواد التعليميّة و الدّورات <br />
-          المرتقبة و الأكثر طلبا
-        </h2>
+        <div className="container">
+          <h2>
+            المواد التعليميّة و الدّورات <br />
+            المرتقبة و الأكثر طلبا
+          </h2>
+        </div>
         <div className={css.triangle}></div>
       </section>
       <section className={css.courses_section}>
-        <ul className={css.list}>
-          <li className={css.item}>
-            <div className={css.img_thumb}>
-              <p className={css.notion}>notion button</p>
-              <img src="" alt="course preview" className={css.image} />
-            </div>
-            <div className={css.info_thumb}>
-              <h4 className={css.preheader}>Preheader</h4>
-              <h3 className={css.header}>Header</h3>
-              <div className={css.wrapper}>
-                <div className={css.rating}>stars</div>
-                <p className={css.descr}>Some description</p>
+        <div className="container">
+          <ul className={css.list}>
+            <li className={css.item}>
+              <div className={css.img_thumb}>
+                <p className={css.notion}>notion notion</p>
+                <img
+                  src={hero_image}
+                  alt="course preview"
+                  className={css.image}
+                />
               </div>
-              <Link className={css.course_button}>
-                Go to course <span className={css.glyph}> &#9664;</span>
-                {/* opposite side: &#9654; */}
-              </Link>
-            </div>
-          </li>
-          <li className={css.item}>
-            <div className={css.img_thumb}>
-              <p className={css.notion}>notion button</p>
-              <img src="" alt="course preview" className={css.image} />
-            </div>
-            <div className={css.info_thumb}>
-              <h4 className={css.preheader}>Preheader</h4>
-              <h3 className={css.header}>Header</h3>
-              <div className={css.wrapper}>
-                <div className={css.rating}>stars</div>
-                <p className={css.descr}>Some description</p>
+              <div className={css.info_thumb}>
+                <h4 className={css.preheader}>Preheader</h4>
+                <h3 className={css.header}>Header</h3>
+                <div className={css.wrapper}>
+                  <div className={css.rating}>stars</div>
+                  <p className={css.descr}>Some description</p>
+                </div>
+                <Link className={css.course_button}>
+                  Go to course <span className={css.glyph}> &#9664;</span>
+                  {/* opposite side: &#9654; */}
+                </Link>
               </div>
-              <Link className={css.course_button}>
-                Go to course <span className={css.glyph}> &#9664;</span>
-                {/* opposite side: &#9654; */}
-              </Link>
-            </div>
-          </li>
-          <li className={css.item}>
-            <div className={css.img_thumb}>
-              <p className={css.notion}>notion button</p>
-              <img src="" alt="course preview" className={css.image} />
-            </div>
-            <div className={css.info_thumb}>
-              <h4 className={css.preheader}>Preheader</h4>
-              <h3 className={css.header}>Header</h3>
-              <div className={css.wrapper}>
-                <div className={css.rating}>stars</div>
-                <p className={css.descr}>Some description</p>
+            </li>
+            <li className={css.item}>
+              <div className={css.img_thumb}>
+                <p className={css.notion}>notion button</p>
+                <img src="" alt="course preview" className={css.image} />
               </div>
-              <Link className={css.course_button}>
-                Go to course <span className={css.glyph}> &#9664;</span>
-                {/* opposite side: &#9654; */}
-              </Link>
-            </div>
-          </li>
-        </ul>
+              <div className={css.info_thumb}>
+                <h4 className={css.preheader}>Preheader</h4>
+                <h3 className={css.header}>Header</h3>
+                <div className={css.wrapper}>
+                  <div className={css.rating}>stars</div>
+                  <p className={css.descr}>Some description</p>
+                </div>
+                <Link className={css.course_button}>
+                  Go to course <span className={css.glyph}> &#9664;</span>
+                  {/* opposite side: &#9654; */}
+                </Link>
+              </div>
+            </li>
+            <li className={css.item}>
+              <div className={css.img_thumb}>
+                <p className={css.notion}>notion button</p>
+                <img src="" alt="course preview" className={css.image} />
+              </div>
+              <div className={css.info_thumb}>
+                <h4 className={css.preheader}>Preheader</h4>
+                <h3 className={css.header}>Header</h3>
+                <div className={css.wrapper}>
+                  <div className={css.rating}>stars</div>
+                  <p className={css.descr}>Some description</p>
+                </div>
+                <Link className={css.course_button}>
+                  Go to course <span className={css.glyph}> &#9664;</span>
+                  {/* opposite side: &#9654; */}
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
       </section>
       <section className={css.learn_more_section}>
-        <h2 className={css.learn_more_header}>
-          نوفّر تجربة تعليميّة تفاعليّة <br />
-          ونفتح آفاقا معرفيّة جديدة
-        </h2>
-        <Link className={css.learn_more_button}>تعرّف أكثر</Link>
+        <div className="container">
+          <h2 className={css.learn_more_header}>
+            نوفّر تجربة تعليميّة تفاعليّة <br />
+            ونفتح آفاقا معرفيّة جديدة
+          </h2>
+          <Link className={css.learn_more_button}>تعرّف أكثر</Link>
+        </div>
       </section>
     </>
   );
