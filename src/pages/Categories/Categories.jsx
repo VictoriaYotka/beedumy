c00403d29f5c300f6ce0b91b424f17ed0c64d903
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-11-30 17:21:34 +0200

    add startLearningTrip section

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import CoursesFilter from "../../components/CoursesFilter/CoursesFilter";
+import StartLearningTrip from "../../components/StartLearningTrip/StartLearningTrip";
 
 const Categories = () => {
   return (
     <>
       <CoursesFilter />
       {/* courses list with CourseCard */}
-      CoursesPage
+      <StartLearningTrip />
     </>
   );
 };
