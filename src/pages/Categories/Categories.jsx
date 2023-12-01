e2c9bd93a5008bad4a24bccabd5f06fe04f2a4f4
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-12-01 11:24:22 +0200

    add faq section

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CoursesFilter from "../../components/CoursesFilter/CoursesFilter";
 import StartLearningTrip from "../../components/StartLearningTrip/StartLearningTrip";
+import FAQ from "../../components/FAQ/FAQ";
 
 const Categories = () => {
   return (
@@ -8,6 +9,7 @@ const Categories = () => {
       <CoursesFilter />
       {/* courses list with CourseCard */}
       <StartLearningTrip />
+      <FAQ />
     </>
   );
 };
