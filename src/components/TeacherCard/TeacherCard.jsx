c97a7f9aed80e3cf054a762a8285fb49efa5590e
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-12-06 12:31:25 +0200

    improve structure of teachers list

---
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -0,0 +1,80 @@
+import css from "./TeacherCard.module.scss";
+import icons from "../../assets/images/icons/icons.svg";
+import hero_image from "../../assets/images/home/hero/image.png";
+import { Link } from "react-router-dom";
+
+const TeacherCard = () => {
+  return (
+    <div className={css.item_thumb}>
+      <div className={css.item}>
+        <Link>
+          <div className={css.teacher_thumb}>
+            <img
+              src={hero_image}
+              alt=""
+              loading="lazy"
+              className={css.teacher_image}
+            />
+            <h4 className={css.name}>Teacher's name</h4>
+            <h5 className={css.occupation}>Occupation</h5>
+            <p className={css.descr}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </Link>
+        <ul className={css.socials}>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className={css.social_link}
+            >
+              <svg className={css.icon}>
+                <use href={icons + "#facebook"}></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className={css.social_link}
+            >
+              <svg className={css.icon}>
+                <use href={icons + "#instagram"}></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className={css.social_link}
+            >
+              <svg className={css.icon}>
+                <use href={icons + "#pinterest"}></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className={css.social_link}
+            >
+              <svg className={css.icon}>
+                <use href={icons + "#twitter"}></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TeacherCard;
