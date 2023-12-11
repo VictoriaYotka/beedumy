6a942afc6eaccb846f7276dd8bb25c0d0fb865f8
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-12-11 17:50:39 +0200

    Updates

--- static/js/103.47e4731f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbirsaplatform=self.webpackChunkbirsaplatform||[]).push([[103],{2103:(s,a,r)=>{r.r(a),r.d(a,{default:()=>t});r(2791);var e=r(184);const t=()=>(0,e.jsx)("div",{children:"NotFound"})}}]);
-//# sourceMappingURL=103.47e4731f.chunk.js.map
