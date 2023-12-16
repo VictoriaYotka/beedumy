d22809239d998061b3c85b7e3c35ac57030f543d
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-12-16 16:46:47 +0200

    improve gh-pages performance

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./components/App/App";
 import "./i18n.js";
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <BrowserRouter basename="birsaplatform">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     {/* </Provider> */}
   </React.StrictMode>
 );
